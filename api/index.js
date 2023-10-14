4f6a8383ff803ccd0bad286376c2670dd1c49573
Anei2017 <patrick_anei@hotmail.com>
2023-10-14 20:47:27 +1000

    create a meddleware function

--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -23,5 +22,14 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
-
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
