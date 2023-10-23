9a8bdac76390ea99973fc7261e4dd4ebd2a46aee
Anei2017 <patrick_anei@hotmail.com>
2023-10-23 14:54:03 +1000

    adding Google functionallity

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,6 +52,7 @@ export const google = async (req, res, next) => {
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
+     
     }
     
   } catch (error) {
